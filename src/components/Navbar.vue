de2d91b3eb595f9fc48355d3b300a6e02233efbc
wesenbergg <b.b.j.1998@gmail.com>
2020-09-09 13:24:59 +0300

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-navbar
+      toggleable="md"
+      type="dark"
+      variant="info">
+    <b-navbar-brand :to="{ name: 'Home' }">Tapahtumakalenterista</b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item :to="{ name: 'AddEvent' }"  >Lisää tapahtuma</b-nav-item>
+      <b-nav-item :to="{ name: 'ListEvent' }">Listaa tapahtuma</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  methods: {
+    AddEvent(){
+      //console.log('Add event')
+
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
