9f2be987208d73cf42de30c9c0524e5fdecafb61
wesenbergg <b.b.j.1998@gmail.com>
2020-09-10 15:43:43 +0300

    lisäys lomake lisätty

--- src/components/AddEvent.vue
+++ src/components/AddEvent.vue
@@ -1,20 +1,47 @@
 <template>
-    <p class="homeText">
-        Syötä tapahtuma
-    </p>
+    <div>
+        <p class="homeText"> Syötä tapahtuma </p>
+        
+        <b-form-input size="lg" class="mb-3" v-model="title" placeholder="Tapahtuman nimi"></b-form-input>
+        <b-form-input size="lg" class="mb-3" v-model="author" placeholder="Järjestäjä"></b-form-input>
+        <b-button size="lg" @click="submit">Tallenna</b-button>
+    </div>
 </template>
 <script>
     export default {
-    name: 'AddEvent'
+    name: 'AddEvent',
+    data() {
+        return {
+            title: "",
+            author: ""
+        }
+    },
+    methods: {
+        submit: function() {
+            fetch('http://localhost:3001/posts', {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(
+                    {
+                        title: this.title,
+                        author: this.author,
+                        date: new Intl.DateTimeFormat('fi-FI').format(new Date()),
+                        id: Math.floor(Math.random() * 99999) + 5
+                    })
+            })
+            .then(() => {
+                this.title = ""
+                this.author = ""
+            })
+        }
+    }
 };
 </script>
 <style scoped>
     .homeText{
-    font-size: 35px;
-    color: red;
-    text-align: center;
-    position: relative;
-    top:30px;
-    text-shadow: 2px 2px 2px gray;
-}
+        font-size: 35px;
+        text-align: left;
+        font-size: 150%;
+        padding-top: 1rem;
+    }
 </style>
