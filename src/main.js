028979fcfac4a901cd0a09bebdf93df94a362b20
wesenbergg <b.b.j.1998@gmail.com>
2020-09-17 15:09:28 +0300

    fixed '#' in route

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(BootstrapVue)
 
-const router = new VueRouter({routes});
+const router = new VueRouter({routes, mode: 'history'});
 new Vue({
     render: h => h(App),
     router,
